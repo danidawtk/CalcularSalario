f71911c29b3d88fb45f0f1595deba067ae89bddd
Donus <Donus@DESKTOP-LLMBNHC>
2021-01-11 15:55:46 +0100

    Calcular salario

---
+++ src/java/servlets/calculoIVA.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import static java.lang.System.out;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Donus
+ */
+public class calculoIVA extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    float tipoIva;
+    float nomemple;
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        tipoIva=Float.ParseFloat(config.getInitParameter("tipoIva"));
+        
+        
+        
+        
+    }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try {
+            float ImporteSinIva= float.parseFloat(request.getParameter("importesiniva"));
+            float cuotaIva = ImporteSinIva * tipoIva /100;
+            float precioConIva = ImporteSinIva - cuotaIva;
+            
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet calculoIVA</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Calculo IVA</h1>");
+            out.println("<h2>Nombre del empleado" + nomemple + "</h1>");
+            out.println("<h2>Salario bruto" + ImporteSinIva + "</h1>");
+            out.println("<h2>Retencion aplicado" + tipoIva + "</h1>");
+            out.println("<h2>Salario neto" + precioConIva + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
